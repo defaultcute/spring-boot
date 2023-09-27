400582a66b9838a10488165bf67e41791d910c2c
defaultcute <asma@ASMAs-MacBook-Pro.local>
2023-09-27 23:16:36 +0200

    add more stuff

--- src/main/java/com/example/springboot/Controller.java
+++ src/main/java/com/example/springboot/Controller.java
@@ -1,13 +1,30 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Controller {
 
+    List<Model> modelList = new ArrayList<>(); //just in memory
+
     @GetMapping("/hello")
-    public String getHello(){
-        return ("hello");
+    public ResponseEntity<List<Model>> getHello(){
+        //Optional<model> optionaluse = new Optional<>;
+       return new ResponseEntity<>(modelList,HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> addMember(@RequestBody Model model){
+        modelList.add(model);
+       // model.setName(name);
+        return ResponseEntity.ok("ok");
     }
 }
---
+++ src/main/java/com/example/springboot/Model.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+public class Model {
+
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
